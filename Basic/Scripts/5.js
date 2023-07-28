a91872b25e2fdfb77cb0cc6b040d31454f37206d
Piyush <75735209+PiyushBL45t@users.noreply.github.com>
2023-07-28 16:29:47 +0530

    Delete Basic/Scripts directory

--- Basic/Scripts/5.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-let myMap = new WeakMap();
-let object_1 = {};
-myMap.set(object_1, "Ball");
-console.log(myMap.get(object_1));
-//# sourceMappingURL=5.js.map
--- Basic/Scripts/6.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-let symbol = Symbol();
-console.log(typeof symbol);
-console.log(symbol.toString());
-//# sourceMappingURL=6.js.map
